d0b74150e7bbc053367c76a52dd047278d986e5a
zyj2018 <14923349@163.com>
2018-10-29 14:04:04 +0800

    test git

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        //GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //// 
+        //// GET: /HelloWorld/ 
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+        //// 
+        //// GET: /HelloWorld/Welcome/  
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+    }
+}
