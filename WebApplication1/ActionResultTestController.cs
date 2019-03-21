2e77e9302b30ea2314527ea1a65143a87a0f8259
zyj2018 <14923349@163.com>
2019-03-21 14:19:34 +0800

    添加项目文件。

---
+++ WebApplication1/ActionResultTestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ActionResultTestController : Controller
+    {
+        public IActionResult ContentTest()
+        {
+            return Content("Content Result Test --ken.io");
+        }
+
+        public IActionResult JsonTest()
+        {
+            return Json(new { Message = "JsonResult Test", Author = "ken.io" });
+        }
+
+        public IActionResult FileTest()
+        {
+            var bytes = Encoding.Default.GetBytes("FileResult Test by ken.io");
+            return File(bytes, "application/text", "filetest.txt");
+        }
+
+        public IActionResult GetId(int id)
+        {
+            return Content($"Action params mapping test by ken.io, id:{id}");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/TaxController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace WebApplication1.Controllers
+{
+    public class TaxController : Controller
+    {
+        // GET: Tax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Tax/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tax/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tax/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tax/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tax/Edit/5
+        [HttpPost]
+        public ActionResult Test(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public string Fill([FromBody] dynamic data)
+
+        {
+
+            string a = data.amount;
+            return a;
+
+        }
+
+        [HttpPost]
+        public string FillEx(string data)
+
+        {
+
+            string a = data;
+            return a;
+
+        }
+
+        [HttpPost]
+        public async Task<string> PostText()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        [HttpPost]
+        public string GetJsonString([FromBody]string content)
+        {
+            return "content: " + content;
+        }
+
+        [HttpPost]
+        [Route("PostX")]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return value;
+        }
+        
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+
+        // POST: Tax/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tax/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tax/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
